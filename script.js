6315f31ca44388f1ad49747c95a4f21220a36339
Ayasha <124164347+Ayasha0@users.noreply.github.com>
2025-02-10 10:51:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+gsap.from(".main .img1", {
+	delay: 0.4,
+	opacity: 0,
+	duration: 1,
+	y: 60,
+});
+
+gsap.from(".main .img2", {
+	delay: 0.4,
+	opacity: 0,
+	duration: 1,
+	x: 40,
+});
+
+gsap.from(".main .img3", {
+	delay: 0.4,
+	opacity: 0,
+	duration: 1,
+	y: -60,
+});
